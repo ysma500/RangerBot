28f240fff92c56fe7f1f599b9f3ddf8c02fc2048
Kitty-kat-lol <32311727+Kitty-kat-lol@users.noreply.github.com>
2017-10-05 11:17:33 -0400

    end of thug

--- MAIN/src/MAIN.cpp
+++ MAIN/src/MAIN.cpp
@@ -12,11 +12,12 @@
 
 // Prototypes de fonctions de threads
 void bumper_watch();
-void PID_watch();
+void timer_watch();
 
 // Prototypes de fonctions (Avancer, Tourner)
 void avancer_distance(int clicGauche, int clicDroit, int distance);
-void rotation_angle(float);
+void rotation_angle(float angle);
+void PID_watch();
 
 int main()
 {
@@ -25,7 +26,7 @@ int main()
 
 	// declarations des threads
 	THREAD thread_bumpers;
-	THREAD thread_PID;
+	THREAD thread_Timer;
 
 	//on choisit le bon mode de gestion d'erreur
 	ERROR_SetMode(LCD_ONLY);
@@ -35,21 +36,51 @@ int main()
 
 	// depart des threads (voir les definitions des fonctions plus bas)
 	thread_bumpers = THREAD_CreateSimple(bumper_watch);
-	thread_PID = THREAD_CreateSimple(PID_watch);
+	thread_Timer = THREAD_CreateSimple(timer_watch);
 
-	//Depart du circuit
-	avancer_distance(int clicGauche, int clicDroit, int 2220);
+	// Depart du circuit
+	avancer_distance(iclicGauche, clicDroit, 2220);
 
+	rotation_angle(90);
 
-	// depart des moteurs en acceleration
-	for (i=2; i<11; i++)
-	{
-		MOTOR_SetSpeed(MOTOR_LEFT,  i*10 + PID);
-		MOTOR_SetSpeed(MOTOR_RIGHT, i*10 + PID);
-		THREAD_MSleep (1000);
-	}
+	avancer_distance(clicGauche, clicDroit, 430);
+
+	rotation_angle(-90);
+
+	avancer_distance(iclicGauche, clicDroit, 450);
+
+	rotation_angle(-90);
+
+	avancer_distance(clicGauche, clicDroit, 430);
+
+	rotation_angle(90);
+
+	avancer_distance(clicGauche, clicDroit, 400);
+
+	rotation_angle(-45);
+
+	avancer_distance(clicGauche, clicDroit, 560);
+
+	rotation_angle(90);
+
+	avancer_distance(clicGauche, clicDroit, 820);
+
+	rotation_angle(-45);
+
+	avancer_distance(clicGauche, clicDroit, 500);
+
+	rotation_angle(-12.5);
+
+	avancer_distance(clicGauche, clicDroit, 460);
+
+	rotation_angle(180);
+
+	AUDIO_SetVolume(50);
+	//AUDIO_PlayMP3File("songno1.mp3");
+
+	avancer_distance(clicGauche, clicDroit, 460);
+
 	LCD_Printf("Le robot a atteint sa pleine vitesse\n");
-
 
 	AUDIO_SetVolume(50);
 
@@ -98,9 +129,30 @@ void bumper_watch()
 	}
 }
 
+void timer_watch()
+{
+	//...
+}
+
 void PID_watch()
 {
 	//...
+}
+
+void rotation_angle(float angle)
+{
+	//...
+}
+
+void avancer_distance(int clicGauche, int clicDroit, int distance);
+{
+		while(clicGauche && clicDroit < distance/7 + 1)
+		{
+			int speed = 0;
+			speed = PID(50);
+			MOTOR_SetSpeed(7, speed);
+			MOTOR_SetSpeed(8, speed);
+		}
 }
 
 
