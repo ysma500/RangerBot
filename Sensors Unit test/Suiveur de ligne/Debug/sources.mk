4d2648f7c2c8735fcce063632155ee6f4029a6c0
Jérémy Soucy <jey_soucy@hotmail.com>
2017-11-06 18:22:34 -0500

    test unitaire suiveur de ligne terminé

--- Sensors Unit test/Suiveur de ligne/Debug/sources.mk
+++ Sensors Unit test/Suiveur de ligne/Debug/sources.mk
@@ -2,20 +2,20 @@
 # Automatically-generated file. Do not edit!
 ################################################################################
 
-O_SRCS := 
-CPP_SRCS := 
 C_UPPER_SRCS := 
-OBJ_SRCS := 
 CXX_SRCS := 
+OBJ_SRCS := 
 C++_SRCS := 
 CC_SRCS := 
-OBJS := 
-C++_DEPS := 
+CPP_SRCS := 
+O_SRCS := 
 CC_DEPS := 
-CPP_DEPS := 
+C++_DEPS := 
 EXECUTABLES := 
-CXX_DEPS := 
 C_UPPER_DEPS := 
+CXX_DEPS := 
+OBJS := 
+CPP_DEPS := 
 
 # Every subdirectory with source files must be described here
 SUBDIRS := \
--- Sensors Unit test/Suiveur de ligne/src/MAIN.cpp
+++ Sensors Unit test/Suiveur de ligne/src/MAIN.cpp
@@ -13,8 +13,25 @@
 // Include Files
 #include <libarmus.h>
 
+//Define Generales
+#define MOTOR_DROIT 8
+#define MOTOR_GAUCHE 7
+
+
+// Define pour la fonction
+#define LINE_DROIT 568
+#define LINE_CENTRE 291
+#define LINE_GAUCHE 150
+#define LINE_DROIT_CENTRE 849
+#define LINE_DROIT_GAUCHE 712
+#define LINE_CENTRE_GAUCHE 436
+#define LINE_NONE 5
+#define LINE_ALL 994
+#define LINE_HYST 10
+
 // Prototypes de fonctions de configs
-void testDeCapteurs(void);
+//void testDeCapteurs(void);
+void det_ligne(void);
 
 int main()
 {
@@ -38,7 +55,8 @@ int main()
 	}
 	
 	//Configuration
-	testDeCapteurs();
+	//testDeCapteurs();
+	det_ligne();
 	
 	LCD_Printf("Fin du test du capteur\n");
 
@@ -53,18 +71,85 @@ int main()
 	return 0;
 }
 
+
 //Debut de la fonction pour la modification des gains a suivre 
+void det_ligne(void)
+{
+	int i = 1;
+	int valeur_lignes = 0;
+	while(i ==1)
+	{
+
+		valeur_lignes = ANALOG_Read(2);
+
+		//Ligne a droite
+		if((valeur_lignes <= (LINE_CENTRE_GAUCHE + LINE_HYST)) && (valeur_lignes >= (LINE_CENTRE_GAUCHE - LINE_HYST)))
+		{
+			MOTOR_SetSpeed(MOTOR_DROIT,70);
+			MOTOR_SetSpeed(MOTOR_GAUCHE,60);
+		}
+		¸<
+		¸
+		¸.
+
+		else if((valeur_lignes < (LINE_DROIT_CENTRE + LINE_HYST)) && (valeur_lignes > (LINE_DROIT_CENTRE - LINE_HYST)))
+		{
+			MOTOR_SetSpeed(MOTOR_DROIT,70);
+			MOTOR_SetSpeed(MOTOR_GAUCHE,60);
+		}
+		else if((valeur_lignes < (LINE_DROIT_GAUCHE + LINE_HYST)) && (valeur_lignes > (LINE_DROIT_GAUCHE - LINE_HYST)))
+
+		{
+			MOTOR_SetSpeed(MOTOR_GAUCHE,70);
+			MOTOR_SetSpeed(MOTOR_DROIT,70);
+		}
+		else if((valeur_lignes < (LINE_DROIT + LINE_HYST)) && (valeur_lignes > (LINE_DROIT - LINE_HYST)))
+		{
+			MOTOR_SetSpeed(MOTOR_GAUCHE,70);
+			MOTOR_SetSpeed(MOTOR_DROIT,65);
+		}
+		else if((valeur_lignes < (LINE_GAUCHE + LINE_HYST)) && (valeur_lignes > (LINE_GAUCHE - LINE_HYST)))
+		{
+			MOTOR_SetSpeed(MOTOR_DROIT,70);
+			MOTOR_SetSpeed(MOTOR_GAUCHE,65);
+		}
+		else if((valeur_lignes < (LINE_CENTRE + LINE_HYST)) && (valeur_lignes > (LINE_CENTRE - LINE_HYST)))
+		{
+			MOTOR_SetSpeed(MOTOR_GAUCHE,70);
+			MOTOR_SetSpeed(MOTOR_DROIT,70);
+		}
+		else if((valeur_lignes < (LINE_NONE + LINE_HYST)) && (valeur_lignes > (LINE_NONE - LINE_HYST)))
+		{
+			MOTOR_SetSpeed(MOTOR_GAUCHE,70);
+			MOTOR_Se0002
+			02tSpeed(MOTOR_DROIT,-70);
+		}
+		else if((valeur_lignes < (LINE_ALL + LINE_HYST)) && (valeur_lignes > (LINE_ALL - LINE_HYST)))
+		{
+			i = 0;
+		}
+	THREAD_MSleep(250);
+	}
+}
+
+
+/*/Debut de la fonction pour la modification des gains a suivre
 void testDeCapteurs(void)
 {
 	int i = 0, j = 0;
+	int valeurLigne = 0;
+
+
 
 	while(i==0 || j==0)
 	{
 
-		//Si la "bumper switch" avant de robus est enclanchee...
-		if(DIGITALIO_Read(BMP_FRONT) && DIGITALIO_Read(BMP_LEFT))	//Configuration 1 de la fonction de test des capteurs
-		{
+		valeurLigne = ANALOG_Read(2);
 
+		//Si la "bumper switch" avant de robus est enclanchee...
+		if(DIGITALIO_Read(BMP_FRONT))	//Configuration 1 de la fonction de test des capteurs
+		{
+			LCD_ClearAndPrint("Valeur analogique des capteurs de ligne : %d\n", valeurLigne);
 		}
 		else if(DIGITALIO_Read(BMP_FRONT) && DIGITALIO_Read(BMP_RIGHT))	//Configuration 2 de la fonction de test des capteurs
 		{
@@ -78,7 +163,7 @@ void testDeCapteurs(void)
 		}
 	}
 }
-
+*/
 
 
 
