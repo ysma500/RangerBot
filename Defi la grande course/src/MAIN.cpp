5d69261341c7f40cc61884634694898f11eb6c48
Malcolm St. John <32340717+ArcticWalrus@users.noreply.github.com>
2017-11-08 14:49:14 -0500

    Commentaires sur code du 5kHz

--- Defi la grande course/src/MAIN.cpp
+++ Defi la grande course/src/MAIN.cpp
@@ -187,10 +187,10 @@ int main()
 		{
 			LCD_Printf("Le signal de 5kHz a ete entendu \n");
 			condition_micro = 1;
-			m_iCouleurDep = get_current_color();
+			m_iCouleurDep = get_current_color(); //trouver la couleur de depart
 			LCD_Printf("La couleur ci-dessous est %d \n", m_iCouleurDep);
 			Avance_BASE();
-		}		
+		}
 	}
 	
 	
