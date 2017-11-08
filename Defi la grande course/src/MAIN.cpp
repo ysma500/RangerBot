0230adcee146607e4deb36fd3df71c55e07cb729
Kitty-kat-lol <32311727+Kitty-kat-lol@users.noreply.github.com>
2017-11-08 13:43:00 -0500

    ecrire fonction avance base

--- Defi la grande course/src/MAIN.cpp
+++ Defi la grande course/src/MAIN.cpp
@@ -515,6 +515,19 @@ void Avance(int iDistance) //Distance en mm
 	MOTOR_SetSpeed(RIGHT_MOTOR, 0);
 }
 
+///Fonction tourne ou avance
+
+void Tourne_gauche_avance()
+{
+	Rotation(45,1);
+	Avance(MIN_DISTANCE);
+}
+
+void Avance_BASE()
+{
+	Avance(MIN_DISTANCE);
+}
+
 
 
 ///****************************************************************************
@@ -686,11 +699,4 @@ int color_Init(int& dev_handle)
 ///*********** FIN Fonctions pour le capteur de couleur ***********************
 ///****************************************************************************
 
-///Fonction tourne aguche et avance apres lecture de couleur
-
-void Tourne_gauche_avance()
-{
-	Rotation(45,1);
-	Avance(MIN_DISTANCE);
-}
 
