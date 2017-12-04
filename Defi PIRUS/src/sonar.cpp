48f125785adeb67f6c67eb8243f8ab19044fc621
Malcolm St. John <32340717+ArcticWalrus@users.noreply.github.com>
2017-12-04 17:57:18 -0500

    Ajout du code de gab dans master
    A travailler

--- Defi PIRUS/src/sonar.cpp
+++ Defi PIRUS/src/sonar.cpp
@@ -17,6 +17,38 @@ float sonar_g()
 	return sonar_gauche;
 }
 
+//Fonctions gab
+int suivre_brigand()
+{
+	float lire_gauche= sonar_g();
+	float lire_droit= sonar_d();
 
+	return 0;
+}
+	
+	
+	
+int sonar_obstacle()
+{
+	float lecture_droit= sonar_d;
+	float lecture_gauche= sonar_g;
+	
+	if (lecture_droit < (last_sonar_d - hyst_sonar))
+	{
+		return 1;
+	}
+	if (lecture_gauche < (last_sonar_g -hyst_sonar))
+	{
+		return 1;
+	}
+	else
+	{
+		return 0;
+	}
+}
+	last_sonar_d = lecture_droit;
+	last_sonar_g = lecture_gauche;
+}
+	
 
 
