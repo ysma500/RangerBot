8e9b4d76682d134d21bae608307d377ee8806505
ysma500 <32311542+ysma500@users.noreply.github.com>
2017-12-04 14:12:51 -0500

    modification au niveau de la gestion du sonar

--- Defi PIRUS/src/MAIN.cpp
+++ Defi PIRUS/src/MAIN.cpp
@@ -254,10 +254,15 @@ int actif()
 		{
 			case RED :
 				LCD_ClearAndPrint("ROUGE Detecte\n");
+				//Recule,tourne à gauche 90 deg. et avance
+				Avance(MIN_DISTANCE/2, RECULE);
+				Rotation(90,LEFT_ROT);
+				Avance(MIN_DISTANCE/2, AVANCE);
 				break;
-			default : 
-				break;
+			default :
+				//Detection d'un intrus
 				
+				break;
 		}
 	}
 	THREAD_MSleep(3000);
--- Defi PIRUS/src/sonar.cpp
+++ Defi PIRUS/src/sonar.cpp
@@ -30,24 +30,27 @@ int suivre_brigand()
 	
 int sonar_obstacle()
 {
-	float lecture_droit= sonar_d;
-	float lecture_gauche= sonar_g;
+	float lecture_droit= sonar_d();
+	float lecture_gauche= sonar_g();
 	
 	if (lecture_droit < (last_sonar_d - hyst_sonar))
 	{
+		last_sonar_d = lecture_droit;
+		last_sonar_g = lecture_gauche;
 		return 1;
 	}
 	if (lecture_gauche < (last_sonar_g -hyst_sonar))
 	{
+		last_sonar_d = lecture_droit;
+		last_sonar_g = lecture_gauche;
 		return 1;
 	}
 	else
 	{
+		last_sonar_d = lecture_droit;
+		last_sonar_g = lecture_gauche;
 		return 0;
 	}
-}
-	last_sonar_d = lecture_droit;
-	last_sonar_g = lecture_gauche;
 }
 	
 
